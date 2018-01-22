27a29208bee09b5efa925cd20e38bbc95ad08f79
Sharmina Akhtar <sakht318@gmail.com>
2018-01-22 10:10:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,8 @@
 class Song
-  attr_accessor :name, :artist, :file_name
-
-  @@all = []
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
-    @file_name = file_name
   end
 
   def self.new_by_filename(file_name)
@@ -13,22 +10,9 @@ class Song
       self.artist.name = file_name.split(" - ")[1]
   end
 
-  def self.find_by_artist(artist)
-    Song.all.select do | song |
-      song.artist == artist
-    end
-  end
-
-  def self.all
-    @@all
-  end
-
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
-  def save
-    @@all << self
-    self
-  end
 end
