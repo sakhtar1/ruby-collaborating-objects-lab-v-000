87849b55b7b05c998a92150c5066ae415f2e7687
Sharmina Akhtar <sakht318@gmail.com>
2018-01-22 10:04:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,38 +1,38 @@
-class Artist 
+class Artist
 
-	attr_accessor :name 
+	attr_accessor :name
 	@@all = []
 
 	def initialize(name)
 		@name = name
 		@songs = []
-	end 
+	end
 
 	def save
-		@@all << self 
-	end 
+		@@all << self
+	end
 
 	def songs
 		@songs
-	end 
+	end
 
 	def add_song(song)
-		@songs << song 
+		@songs << song
 	end
 
 	def self.find_or_create_by_name(name)
 		@@all.each  do |artist|
-			if artist.name == name 
-				return artist 
-			else 
+			if artist.name == name
+				return artist
+			else
 				return self.new(name)
-			end 
-		end 
-	end 
+			end
+		end
+	end
 
-	def self.all 
-		@@all 
-	end 
+	def self.all
+		@@all
+	end
 
 
 end 
